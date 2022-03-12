61b741b06e99ef08191e12cb6ebec8ceb1017812
hugosenari <hugosenari@gmail.com>
2022-03-12 00:44:44 -0300

    chore: re index bcrypt

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Wraps the bcrypt (blowfish) library for creating encrypted hashes (useful for passwords)'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."bcrypt-master".url = "path:./master";
+  inputs."bcrypt-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bcrypt-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Wraps the bcrypt (blowfish) library for creating encrypted hashes (useful for passwords)'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-bcrypt-master.flake = false;
+  inputs.src-bcrypt-master.owner = "ithkuil";
+  inputs.src-bcrypt-master.ref   = "refs/heads/master";
+  inputs.src-bcrypt-master.repo  = "bcryptnim";
+  inputs.src-bcrypt-master.type  = "github";
+  
+  inputs."nimrod".dir   = "nimpkgs/n/nimrod";
+  inputs."nimrod".owner = "riinr";
+  inputs."nimrod".ref   = "flake-pinning";
+  inputs."nimrod".repo  = "flake-nimble";
+  inputs."nimrod".type  = "github";
+  inputs."nimrod".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrod".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-bcrypt-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-bcrypt-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
